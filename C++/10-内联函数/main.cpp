70a875d7eafc2901c38ffe9b307573f799ce0113
wangfuhao <wangfuhao@dongyin.net.cn>
2019-05-27 20:07:22 +0800

    删除修改

--- C++/10-内联函数/main.cpp
+++ C++/10-内联函数/main.cpp
@@ -9,18 +9,31 @@
 #include <iostream>
 using namespace std;
 
+//inline的功能和宏类似,但是inline在写的时候,会有语法检测
+//#define SUM(a,b) ((a)+(b))
+
 #define SQUARE(X) X*X
 
-//内联函数 性能很高,会直接把编译好的二进制代码嵌入到你调用的代码处,不是函数调用了
+//内联函数 性能很高,会直接把编译好的二进制代码嵌入到你调用的代码处,不是函数调用了,相当于直接把函数里的代码,直接复制过来
 //显示内联函数:通过inline关键字声明的内联函数叫显示内联
+//如果函数的代码很多,或者是递归,即使你声明了为inline,但编译器也有可能不会帮你去做转化
 inline int square(int X) {
     return X * X;
 }
+
+//inline既可以只加到声明,也可以只加到实现,最好两个都加
+inline void test();
 
 int main(int argc, const char * argv[]) {
     cout <<  SQUARE(3) << endl;
     cout <<  SQUARE(2+3) << endl; //11 很明显不是想要的结果 这是宏的缺陷 2 + 3 * 2 + 3 = 11
     
+    test();
+    
     cout << square(2+3) << endl;
     return 0;
+}
+
+void test() {
+    cout <<"测试inline" <<endl;
 }
--- C++/62-externC/math.h
+++ C++/62-externC/math.h
@@ -9,7 +9,7 @@
 #ifndef math_h //防止重复包含
 #define math_h
 
-#ifdef __cplusplus //条件编译,如果是c++文件调用,才参与p编译
+#ifdef __cplusplus //条件编译,如果是c++文件调用,才参与编译
 extern "C" {
 #endif
     int sum(int v1,int v2);
